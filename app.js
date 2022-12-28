75a1bfbb206f15a65301cdaf2dcd1cd7a4fc2a1d
danyatcode <danyatd22@gmail.com>
2022-12-29 00:24:40 +0200

    Update

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.toggle-menu').addEventListener('click', function() {
+    document.querySelector('.navigation').classList.toggle('active')
+})
+document.querySelector('.close-nav').addEventListener('click', function() {
+    document.querySelector('.navigation').classList.toggle('active')
+})
